9e98a6bf04262ee7ad7d277bb2fe0f3e0590d02b
Soab42 <108150913+Soab42@users.noreply.github.com>
2023-12-18 19:59:04 +0600

    update showing counted items

--- src/components/post/Answer.jsx
+++ src/components/post/Answer.jsx
@@ -2,9 +2,12 @@ import Comments from "./Comments";
 import Author from "./Author";
 import AnswerForm from "../form/AnswerForm";
 import TextWithMarkup from "../code/TextWithMarkup";
+import { useState } from "react";
 
 export default function Answer({ answer, status }) {
   // Object.values(answer).map((ans) => console.log(ans));
+  const [showAnswer, setShowAnswer] = useState(4);
+
   return (
     <div className="answer flex flex-col gap-1 text-slate-400">
       <div
@@ -13,33 +16,40 @@ export default function Answer({ answer, status }) {
       >
         Answer
       </div>
-      {Object?.keys(answer)?.map((key, i) => {
-        return (
-          <div key={answer[key]?.time} className="flex flex-col gap-1 mt-1">
-            <div className="font-bold text-xl">#{i + 1}</div>
-            {/* <div>{ans.text}</div> */}
-            <TextWithMarkup text={answer[key]?.text} />
-            <div className="h-2"></div>
-            <Comments
-              comments={answer[key]?.comment}
-              answerId={key}
-              status={status}
-            />
-            <div className="h-2"></div>
-
-            <div>
-              <Author
-                author={answer[key]?.author}
-                time={answer[key]?.time}
-                icon={answer[key]?.icon}
-                ans={true}
+      {Object?.keys(answer)
+        ?.slice(-showAnswer)
+        .map((key, i) => {
+          return (
+            <div key={answer[key]?.time} className="flex flex-col gap-1 mt-1">
+              <div className="font-bold text-xl">#{i + 1}</div>
+              {/* <div>{ans.text}</div> */}
+              <TextWithMarkup text={answer[key]?.text} />
+              <div className="h-2"></div>
+              <Comments
+                comments={answer[key]?.comment}
+                answerId={key}
+                status={status}
               />
+              <div className="h-2"></div>
+
+              <div>
+                <Author
+                  author={answer[key]?.author}
+                  time={answer[key]?.time}
+                  icon={answer[key]?.icon}
+                  ans={true}
+                />
+              </div>
+              <hr />
+              <div className="h-5"></div>
             </div>
-            <hr />
-            <div className="h-5"></div>
-          </div>
-        );
-      })}
+          );
+        })}
+      {answer && Object?.values(answer).length > showAnswer && (
+        <button onClick={() => setShowAnswer((prev) => prev + 4)}>
+          show more answer
+        </button>
+      )}
       <div className="anserForm">{!status && <AnswerForm />}</div>
     </div>
   );
--- src/components/post/Comments.jsx
+++ src/components/post/Comments.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import CommentForm from "../form/CommentForm";
 
 export default function Comments({ comments, answerId, status }) {
   // console.log(answerId);
+  const [showComment, setShowComment] = useState(5);
 
   return (
     <div className="text-gray-500">
@@ -14,17 +16,24 @@ export default function Comments({ comments, answerId, status }) {
       </div>
       <div className="flex flex-col gap-1 mb-2">
         {comments &&
-          Object?.values(comments)?.map((comment, i) => {
-            return (
-              <div key={i} className="bg-slate-600/20 p-2 mt-0.5 flex gap-2">
-                <span className="text-green-600 flex">
-                  {comment.author.name}:
-                </span>
+          Object?.values(comments)
+            ?.slice(-showComment)
+            .map((comment, i) => {
+              return (
+                <div key={i} className="bg-slate-600/20 p-2 mt-0.5 flex gap-2">
+                  <span className="text-green-600 flex">
+                    {comment.author.name}:
+                  </span>
 
-                {comment.text}
-              </div>
-            );
-          })}
+                  {comment.text}
+                </div>
+              );
+            })}
+        {comments && Object?.values(comments).length > showComment && (
+          <button onClick={() => setShowComment((prev) => prev + 5)}>
+            show more comments
+          </button>
+        )}
       </div>
       {!status && <CommentForm answerId={answerId} />}
     </div>
